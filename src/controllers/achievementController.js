2bf5e53aab1649e4ef86b2fd9fa3a89e5711c30e
EngrHenry111 <henryengrakpan@gmail.com>
2026-04-04 11:10:02 +0100

    achievment

--- src/controllers/achievementController.js
+++ src/controllers/achievementController.js
@@ -1,7 +1,14 @@
 import Achievement from "../models/Achievements.js";
 
 export const create = async (req, res) => {
-  res.json(await Achievement.create(req.body));
+  const {title, description, year} = req.body;
+
+  const achievement = await Achievement.create({
+    title,
+    description,
+    year
+  });
+  res.json(achievement)
 };
 
 export const getAll = async (req, res) => {
@@ -9,7 +16,13 @@ export const getAll = async (req, res) => {
 };
 
 export const update = async (req, res) => {
-  res.json(await Achievement.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+  const { title, description, year} = req.body;
+
+  const updated = await Achievement.findByIdAndUpdate(
+    req.params.id, {title, description, year},
+    {new:true}
+  );
+  res.json(updated);
 };
 
 export const remove = async (req, res) => {
